303473e76e6b4604103b37947f1c132abf0753e0
Levi <levisenandrade@gmail.com>
2026-09-16 22:45:36 -0300

    Classe escolha atualizada
    apesar dessa alteração ainda vai ser preciso incluir mesmo a classe Efeito, porque podem haver alterações em atributos diferentes simultaneamente (adição/perda de um item + alteração de atributo do protagonista/confiança de um NPC)

--- src/Escolha.java
+++ src/Escolha.java
@@ -1,12 +1,34 @@
 public class Escolha {
 
     private String texto;
+    private String atributoAfetado;
+    private int valorAlteracao;
 
     public Escolha(String texto) {
         this.texto = texto;
+        this.atributoAfetado = null;
+        this.valorAlteracao = 0;
+    }
+
+    public Escolha(String texto, String atributoAfetado, int valorAlteracao) {
+        this.texto = texto;
+        this.atributoAfetado = atributoAfetado;
+        this.valorAlteracao = valorAlteracao;
     }
 
     public String getTexto() {
         return texto;
+    }
+
+    public String getAtributoAfetado() {
+        return atributoAfetado;
+    }
+
+    public int getValorAlteracao() {
+        return valorAlteracao;
+    }
+
+    public boolean afetaAtributo() {
+        return atributoAfetado != null;
     }
 }
