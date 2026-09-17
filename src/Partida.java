0c297639bc9d0b9bdc86f4e1f1676ae04c4ac9ed
Levi <levisenandrade@gmail.com>
2026-09-16 22:43:16 -0300

    Opções iniciais fazem parte do menu

--- src/Partida.java
+++ src/Partida.java
@@ -1,7 +1,6 @@
 public class Partida {
     public static void main(String[] args) {
         Controlador save1 = new Controlador();
-        save1.opcoesIniciais();//Iniciei um jogo novo (Criação do personagem+ prologo)
-        //Primeiro Cenário+escolhas
+        save1.menuInicial();
     }
 }
