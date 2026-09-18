677aa9fa3ef95c28d6218336d61e38963552910e
Luis Felipe Santana Batista <luisfelipesb11@gmail.com>
2026-09-18 17:09:13 -0300

    Update Protagonista.java

--- src/Protagonista.java
+++ src/Protagonista.java
@@ -1,5 +1,6 @@
 public class Protagonista extends Personagem {
     private Atributo[] atributos;
+    private Inventario inventario;
 
     public Protagonista(String nome, int idade, String genero) {
         super(nome, idade, genero);
@@ -32,5 +33,20 @@ public class Protagonista extends Personagem {
             }
         }
         return 0;
+    }
+
+    public void criarInventario(){
+        Item cartaoIml = new Item("Cartão do IML", "Acesso", "Um cartão de acesso que permite acessar o IML da cidade", 0);
+        Item chaveMestra = new Item("Chave Mestra do Cartório", "Acesso", "Uma chave estranha com o símbolo do cartório central que permite acessar o prédio de registros", 0);
+        Item cartaoPrefeitura = new Item("Cartão de Acesso da Prefeitura", "Acesso", "Um cartão magnético que permite acessar o prédio da prefeitura municipal", 0);
+        Item chaveCofrePrefeitura = new Item("Chave do Cofre da Prefeitura", "Acesso", "Uma chave de segurança que permite acessar o cofre e o arquivo restrito", 0);
+        Item peDeCabra = new Item("Pé de Cabra", "Acesso", "Uma ferramenta pesada que permite forçar e abrir portas e acessos bloqueados", 0);
+
+        inventario.cadastrarItem(cartaoIml);
+        inventario.cadastrarItem(chaveMestra);
+        inventario.cadastrarItem(cartaoPrefeitura);
+        inventario.cadastrarItem(chaveCofrePrefeitura);
+        inventario.cadastrarItem(peDeCabra);
+
     }
 }
