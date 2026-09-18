f63195ef587320f5031964284337d84374c1db63
Luis Felipe Santana Batista <luisfelipesb11@gmail.com>
2026-09-18 18:47:32 -0300

    Update Cena.java

--- src/Cena.java
+++ src/Cena.java
@@ -1,78 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Cena {
 
-    private Dialogo[] arrayDialogos;
-    private int dialogoAtual;
+    private List<Dialogo> dialogos;
 
-    // Construtor com tamanho padrão de diálogos
     public Cena() {
-        this.arrayDialogos = new Dialogo[5];
-        this.dialogoAtual = 0;
+        this.dialogos = new ArrayList<>();
     }
-
-    // Construtor para quantidades maiores de diálogos
-    public Cena(int quantidadeDialogos) {
-        this.arrayDialogos = new Dialogo[quantidadeDialogos];
-        this.dialogoAtual = 0;
-    }
-
-    // Criação de diálogo que possui somente texto
-    public void adicionaDialogoSemOpcoes(
-            Personagem personagem,
-            String texto
-    ) {
-
-        if (dialogoAtual < arrayDialogos.length) {
-
-            arrayDialogos[dialogoAtual] =
-                    new Dialogo(
-                            personagem,
-                            texto
-                    );
-
-            dialogoAtual += 1;
-        }
-    }
-
-    // Criação de diálogo que possui texto, opções e consequências
-    public void adicionaDialogoComOpcoes(
-            Personagem personagem,
-            String texto,
-            Escolha escolha1,
-            Escolha escolha2,
-            Escolha escolha3
-    ) {
-
-        if (dialogoAtual < arrayDialogos.length) {
-
-            arrayDialogos[dialogoAtual] =
-                    new Dialogo(
-                            personagem,
-                            texto,
-                            escolha1,
-                            escolha2,
-                            escolha3
-                    );
-
-            dialogoAtual += 1;
-        }
-    }
-
-    public void executaDialogos() {
-
-        for (int i = 0; i < dialogoAtual; i++) {
-
-            Dialogo dialogo = arrayDialogos[i];
-
-            dialogo.executarDialogo();
-        }
-    }
-
-    public Dialogo getDialogo(int indice) {
-        return arrayDialogos[indice];
+    
+    public void adicionaDialogo(Dialogo dialogo) {
+        this.dialogos.add(dialogo);
     }
 
     public int getQuantidadeDialogos() {
-        return dialogoAtual;
+        return this.dialogos.size();
     }
-
 }
