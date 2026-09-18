e49bde114799860ff5485765c3173f89140fa31c
Luis Felipe Santana Batista <luisfelipesb11@gmail.com>
2026-09-18 17:05:05 -0300

    Update Item.java

--- src/Item.java
+++ src/Item.java
@@ -1,42 +1,43 @@
 public class Item {
-    private String item_nome;
-    private String item_tipo;
-    private String item_descricao;
-    private int item_id;
-    private int item_quant;
+    private String itemNome;
+    private String itemTipo;
+    private String itemDescricao;
+    private int itemID;
+    private boolean itemStatus;
 
-    public Item(String item_nome, String item_tipo, String item_descricao, int item_id, int item_quant) {
-        this.item_nome = item_nome;
-        this.item_tipo = item_tipo;
-        this.item_descricao = item_descricao;
-        this.item_id = item_id;
-        this.item_quant = item_quant;
+    public Item(String itemNome, String itemTipo, String itemDescricao, int itemId) {
+        this.itemNome = itemNome;
+        this.itemTipo = itemTipo;
+        this.itemDescricao = itemDescricao;
+        this.itemID = itemId;
+        this.itemStatus = false; // Inicia fora do inventário
     }
 
     public String getNome() {
-        return this.item_nome;
-    }
-
-    public int getQuant() {
-        return this.item_quant;
+        return this.itemNome;
     }
 
     public String getDescricao() {
-        return this.item_descricao;
+        return this.itemDescricao;
     }
 
-    public void Consumir(int quantidade) {
-        if (item_quant >= quantidade) {
-            this.item_quant -= quantidade;
-            System.out.println("Você Consumiu: " + getNome());
-        } else {
-            System.out.println("Você não tem " + getNome() + " suficientes para isso!");
+    public int getItemID() {
+        return this.itemID;
+    }
+
+    public boolean getItemStatus() {
+        return this.itemStatus;
+    }
+
+    public void consumir() {
+        if (this.itemStatus) {
+            this.itemStatus = false;
         }
     }
 
-    public void Incrementar(int quantidade) {
-        //if(item<0) tem que criar o item aq? ou só pode incrementar se já existir o item no inventário
-        this.item_quant += quantidade;
-        System.out.println("Você coleutou " + quantidade + getNome());
+    public void adicionar() {
+        if (!this.itemStatus) {
+            this.itemStatus = true;
+        }
     }
 }
