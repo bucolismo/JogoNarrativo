3ca365f9df4b6bc567d96303f499fedf5bbf1087
Levi <levisenandrade@gmail.com>
2026-09-19 10:38:51 -0300

    adiciona métodos para restrição de acesso de confiança

--- src/Escolha.java
+++ src/Escolha.java
@@ -1,15 +1,22 @@
 import java.util.ArrayList;
 
 public class Escolha {
-
     private String texto;
     private ArrayList<Efeito> efeitos;
     private int requisitoItem;
+    private String requisitoAtributo;
+    private int valorRequisitoAtributo;
+    private NPC requisitoNpc;
+    private int confiancaMinima;
 
     public Escolha(String texto) {
         this.texto = texto;
         this.efeitos = new ArrayList<>();
         this.requisitoItem = -1;
+        this.requisitoAtributo = null;
+        this.valorRequisitoAtributo = 0;
+        this.requisitoNpc = null;
+        this.confiancaMinima = 0;
     }
 
     public Escolha(String texto, Efeito efeito) {
@@ -17,12 +24,20 @@ public class Escolha {
         this.efeitos = new ArrayList<>();
         this.efeitos.add(efeito);
         this.requisitoItem = -1;
+        this.requisitoAtributo = null;
+        this.valorRequisitoAtributo = 0;
+        this.requisitoNpc = null;
+        this.confiancaMinima = 0;
     }
 
     public Escolha(String texto, ArrayList<Efeito> efeitos) {
         this.texto = texto;
         this.efeitos = efeitos;
         this.requisitoItem = -1;
+        this.requisitoAtributo = null;
+        this.valorRequisitoAtributo = 0;
+        this.requisitoNpc = null;
+        this.confiancaMinima = 0;
     }
 
     public void defineRequisitoItem(int id) {
@@ -31,6 +46,36 @@ public class Escolha {
 
     public int getRequisitoItem() {
         return requisitoItem;
+    }
+
+    public void defineRequisitoAtributo(String atributo, int valor) {
+        this.requisitoAtributo = atributo;
+        this.valorRequisitoAtributo = valor;
+    }
+
+    public String getRequisitoAtributo() {
+        return requisitoAtributo;
+    }
+
+    public int getValorRequisitoAtributo() {
+        return valorRequisitoAtributo;
+    }
+
+    /**
+     * módulos de requisito (restrição de acesso) da interação do protagonista com a confiança do NPC
+     */
+
+    public void defineRequisitoConfianca(NPC npc, int confiancaMinima) {
+        this.requisitoNpc = npc;
+        this.confiancaMinima = confiancaMinima;
+    }
+
+    public NPC getRequisitoNpc() {
+        return requisitoNpc;
+    }
+
+    public int getConfiancaMinima() {
+        return confiancaMinima;
     }
 
     public String getTexto() {
