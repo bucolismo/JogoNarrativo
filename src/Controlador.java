f28fa57000da1b64842cf4c80f25d322d1769c04
Levi <levisenandrade@gmail.com>
2026-09-19 03:02:07 -0300

    controlador já com restrição de escolha por item
    falta adicionar a restrição de atributo/confiança

--- src/Controlador.java
+++ src/Controlador.java
@@ -47,22 +47,44 @@ public class Controlador {
         String genero = menu.recebeGenero();
 
         Protagonista protagonista = new Protagonista(nome, idade, genero);
-
         protagonista.cadastrarItens();
 
-        // inicialização dos NPCs
+        Personagem narrador = new Personagem("Narrador", 0, "Neutro");
+
         NPC npc1 = new NPC("Marcos", 30, "Masculino", 50);
         NPC npc2 = new NPC("Angela", 40, "Feminino", 50);
         NPC npc3 = new NPC("Henrique", 25, "Masculino", 50);
         NPC npc4 = new NPC("Lourdes", 35, "Feminino", 50);
         NPC npc5 = new NPC("Almeida", 50, "Masculino", 50);
 
-        Personagem narrador =
-                new Personagem(
-                        "Narrador",
-                        0,
-                        "Neutro"
-                );
+        protagonista.getInventario().adicionarItemPorId(5);
+
+        Escolha testeItem = new Escolha("1 - Abrir a porta com o pé de cabra.");
+        testeItem.defineRequisitoItem(5);
+
+        Cena cenaTeste = new Cena();
+
+        cenaTeste.adicionaDialogo(
+                new Dialogo(
+                        narrador,
+                        "Você encontra uma porta trancada.",
+                        testeItem,
+                        new Escolha("2 - Ir embora."),
+                        new Escolha("3 -Observar a porta.") // só pra fechar os 3 argumentos
+                )
+        );
+
+        System.out.println(
+                "Possui pé de cabra: " +
+                        protagonista.getInventario().possuiItemPorId(5)
+        );
+
+        executaCena(cenaTeste, protagonista);
+
+        System.out.println(
+                "Possui pé de cabra depois: " +
+                        protagonista.getInventario().possuiItemPorId(5)
+        );
 
         ConstrutorDeCenas construtor = new ConstrutorDeCenas();
 
@@ -75,10 +97,7 @@ public class Controlador {
         executaCena(prologo, protagonista);
     }
 
-    public void executaCena(
-            Cena cena,
-            Protagonista protagonista
-    ) {
+    public void executaCena(Cena cena, Protagonista protagonista) {
 
         for (Dialogo dialogo : cena.getDialogos()) {
 
@@ -86,40 +105,38 @@ public class Controlador {
 
             if (dialogo.possuiOpcoes()) {
 
-                int escolha = menu.recebeEscolha();
+                Escolha escolhaSelecionada;
 
-                Escolha escolhaSelecionada =
-                        dialogo.getEscolha(escolha);
+                do {
+                    int escolha = menu.recebeEscolha();
+                    escolhaSelecionada = dialogo.getEscolha(escolha);
 
-                System.out.println(
-                        escolhaSelecionada.getTexto()
-                );
+                    int requisito = escolhaSelecionada.getRequisitoItem();
 
-                for (Efeito efeito :
-                        escolhaSelecionada.getEfeitos()) {
+                    if (requisito != -1 &&
+                            !protagonista.getInventario().possuiItemPorId(requisito)) {
+
+                        System.out.println("Você não tem o item necessário para essa escolha.");
+                        escolhaSelecionada = null;
+                    }
+
+                } while (escolhaSelecionada == null);
+
+                System.out.println(escolhaSelecionada.getTexto());
+
+                for (Efeito efeito : escolhaSelecionada.getEfeitos()) {
 
                     if (efeito.getTipo().equals("ATRIBUTO")) {
-
-                        protagonista.alteraAtributo(
-                                efeito.getAlvo(),
-                                efeito.getValor()
-                        );
+                        protagonista.alteraAtributo(efeito.getAlvo(), efeito.getValor());
 
                     } else if (efeito.getTipo().equals("CONFIANCA")) {
-
-                        efeito.getNpc().alterarConfianca(
-                                efeito.getValor()
-                        );
+                        efeito.getNpc().alterarConfianca(efeito.getValor());
 
                     } else if (efeito.getTipo().equals("ADICIONAR_ITEM")) {
-
-                        protagonista.getInventario()
-                                .adicionarItemPorId(efeito.getValor());
+                        protagonista.getInventario().adicionarItemPorId(efeito.getValor());
 
                     } else if (efeito.getTipo().equals("REMOVER_ITEM")) {
-
-                        protagonista.getInventario()
-                                .removerItemPorId(efeito.getValor());
+                        protagonista.getInventario().removerItemPorId(efeito.getValor());
                     }
                 }
             }
