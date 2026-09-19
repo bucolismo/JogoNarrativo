b9beccb8bbe62fe19154b8ceaccb64dcfd967053
Levi <levisenandrade@gmail.com>
2026-09-19 02:01:55 -0300

    correção nos IDs dos itens
    itens estavam todos com o ID como 0, porém eles precisam ser diferentes entre si para o uso próprio nos métodos

--- src/Protagonista.java
+++ src/Protagonista.java
@@ -10,6 +10,8 @@ public class Protagonista extends Personagem {
         atributos[0] = new Atributo("Razão", 50, 100);
         atributos[1] = new Atributo("Paranoia", 50, 100);
         atributos[2] = new Atributo("Violencia", 50, 100);
+
+        inventario = new Inventario();
     }
 
     public void alteraAtributo(String nomeAtributo, int quantidade) {
@@ -35,18 +37,22 @@ public class Protagonista extends Personagem {
         return 0;
     }
 
-    public void criarInventario(){
-        Item cartaoIml = new Item("Cartão do IML", "Acesso", "Um cartão de acesso que permite acessar o IML da cidade", 0);
-        Item chaveMestra = new Item("Chave Mestra do Cartório", "Acesso", "Uma chave estranha com o símbolo do cartório central que permite acessar o prédio de registros", 0);
-        Item cartaoPrefeitura = new Item("Cartão de Acesso da Prefeitura", "Acesso", "Um cartão magnético que permite acessar o prédio da prefeitura municipal", 0);
-        Item chaveCofrePrefeitura = new Item("Chave do Cofre da Prefeitura", "Acesso", "Uma chave de segurança que permite acessar o cofre e o arquivo restrito", 0);
-        Item peDeCabra = new Item("Pé de Cabra", "Acesso", "Uma ferramenta pesada que permite forçar e abrir portas e acessos bloqueados", 0);
+    public void cadastrarItens(){
+        Item cartaoIml = new Item("Cartão do IML", "Acesso", "Um cartão de acesso que permite acessar o IML da cidade", 1);
+        Item chaveMestra = new Item("Chave Mestra do Cartório", "Acesso", "Uma chave estranha com o símbolo do cartório central que permite acessar o prédio de registros", 2);
+        Item cartaoPrefeitura = new Item("Cartão de Acesso da Prefeitura", "Acesso", "Um cartão magnético que permite acessar o prédio da prefeitura municipal", 3);
+        Item chaveCofrePrefeitura = new Item("Chave do Cofre da Prefeitura", "Acesso", "Uma chave de segurança que permite acessar o cofre e o arquivo restrito", 4);
+        Item peDeCabra = new Item("Pé de Cabra", "Acesso", "Uma ferramenta pesada que permite forçar e abrir portas e acessos bloqueados", 5);
 
         inventario.cadastrarItem(cartaoIml);
         inventario.cadastrarItem(chaveMestra);
         inventario.cadastrarItem(cartaoPrefeitura);
         inventario.cadastrarItem(chaveCofrePrefeitura);
         inventario.cadastrarItem(peDeCabra);
-
     }
+
+    public Inventario getInventario() {
+        return inventario;
+    }
+
 }
