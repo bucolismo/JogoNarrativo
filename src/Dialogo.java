4b54110840519023e2a3b8ded3cbac8f7943201b
Luis Felipe Santana Batista <luisfelipesb11@gmail.com>
2026-09-20 09:38:52 -0300

    Update Dialogo.java
    Dialogo agora permite uma quantidade fluida de escolhas

--- src/Dialogo.java
+++ src/Dialogo.java
@@ -1,29 +1,42 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Dialogo {
 
     private Personagem personagem;
     private String texto;
-    private Escolha[] escolhas;
+    private List<Escolha> escolhas;
 
-    // Construtor de diálogo sem opções
+    // Construtor 1: Diálogo sem opções (apenas narrativa/fala)
     public Dialogo(Personagem personagem, String texto) {
         this.personagem = personagem;
         this.texto = texto;
-
         this.escolhas = null;
     }
 
-    // Construtor de diálogo com opções
-    public Dialogo(Personagem personagem, String texto, Escolha escolha1, Escolha escolha2, Escolha escolha3) {
+
+    public Dialogo(Personagem personagem, String texto, Escolha... escolhas) {
         this.personagem = personagem;
         this.texto = texto;
 
-        this.escolhas = new Escolha[3];
-
-        this.escolhas[0] = escolha1;
-        this.escolhas[1] = escolha2;
-        this.escolhas[2] = escolha3;
+        if (escolhas != null && escolhas.length > 0) {
+            this.escolhas = new ArrayList<>(Arrays.asList(escolhas));
+        } else {
+            this.escolhas = null;
+        }
     }
+
+    // Retorna o Arraydinâmico como um array normal
     public Escolha[] getEscolhas() {
+        if (escolhas == null) {
+            return null;
+        }
+        return escolhas.toArray(new Escolha[0]);
+    }
+
+
+    public List<Escolha> getListaEscolhas() {
         return escolhas;
     }
 
@@ -36,10 +49,22 @@ public class Dialogo {
     }
 
     public boolean possuiOpcoes() {
-        return escolhas != null;
+        return escolhas != null && !escolhas.isEmpty();
     }
 
+    // Robocop Fortaleceu aqui, simula o acesso por indice do array comum
     public Escolha getEscolha(int indice) {
-        return escolhas[indice - 1];
+        if (escolhas == null || indice < 1 || indice > escolhas.size()) {
+            return null;
+        }
+        return escolhas.get(indice - 1);
+    }
+
+    public void adicionaEscolha(Escolha novaEscolha) {
+        this.escolhas.add(novaEscolha);
+    }
+
+    public int getQuantidadeEscolhas() {
+        return escolhas.size();
     }
 }
