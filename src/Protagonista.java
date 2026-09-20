308286584eff51df0e373c36dd57e23c5eeaa2f5
Luis Felipe Santana Batista <luisfelipesb11@gmail.com>
2026-09-20 10:02:26 -0300

    Update Protagonista.java
    Adicionei mais alguns intem que aparecem no AtoI

--- src/Protagonista.java
+++ src/Protagonista.java
@@ -37,18 +37,25 @@ public class Protagonista extends Personagem {
         return 0;
     }
 
-    public void cadastrarItens(){
+ public void cadastrarItens(){
         Item cartaoIml = new Item("Cartão do IML", "Acesso", "Um cartão de acesso que permite acessar o IML da cidade", 1);
         Item chaveMestra = new Item("Chave Mestra do Cartório", "Acesso", "Uma chave estranha com o símbolo do cartório central que permite acessar o prédio de registros", 2);
         Item cartaoPrefeitura = new Item("Cartão de Acesso da Prefeitura", "Acesso", "Um cartão magnético que permite acessar o prédio da prefeitura municipal", 3);
         Item chaveCofrePrefeitura = new Item("Chave do Cofre da Prefeitura", "Acesso", "Uma chave de segurança que permite acessar o cofre e o arquivo restrito", 4);
         Item peDeCabra = new Item("Pé de Cabra", "Acesso", "Uma ferramenta pesada que permite forçar e abrir portas e acessos bloqueados", 5);
+        Item pistaDeJonas = new Item("Pista de Jonas","Acesso","Uma pista que permite seguir outro caminho",6);
+        Item pistaManequim = new Item("Pista do Manequim","Acesso","Uma pista plantada por alguém em um manequi",7);
+        Item laudoAdulterado = new Item("Laudo Adulterado","Evidência","Um laudo adulterado do caso de Gabriel",8);
 
         inventario.cadastrarItem(cartaoIml);
         inventario.cadastrarItem(chaveMestra);
         inventario.cadastrarItem(cartaoPrefeitura);
         inventario.cadastrarItem(chaveCofrePrefeitura);
         inventario.cadastrarItem(peDeCabra);
+        inventario.cadastrarItem(pistaDeJonas);
+        inventario.cadastrarItem(pistaManequim);
+        inventario.cadastrarItem(laudoAdulterado);
+
     }
 
     public Inventario getInventario() {
