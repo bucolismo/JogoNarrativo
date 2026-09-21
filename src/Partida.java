b2bf62ccff6596f64bf529ab42adb592b709d8f9
Levi <levisenandrade@gmail.com>
2026-09-21 00:01:32 -0300

    documentação da classe partida

--- src/Partida.java
+++ src/Partida.java
@@ -1,3 +1,9 @@
+/**
+ * Classe responsável por iniciar a execução do jogo.
+ *
+ * @author Levi Sena Andrade
+ */
+
 public class Partida {
     public static void main(String[] args) {
         Controlador save1 = new Controlador();
