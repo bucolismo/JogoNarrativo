cf2f0b3f9dace42d702916f2edb616b12d1fc278
Levi <levisenandrade@gmail.com>
2026-09-20 23:43:50 -0300

    documentação

--- src/Efeito.java
+++ src/Efeito.java
@@ -1,3 +1,9 @@
+/**
+ * Representa um efeito que pode ser aplicado ao estado do jogo
+ *
+ * Um efeito pode alterar um atributo do protagonista, modificar a confiança
+ * de um NPC ou adicionar um item ao inventário do protagonista.
+ */
 public class Efeito {
 
     private String tipo;
@@ -7,7 +13,14 @@ public class Efeito {
     private Protagonista protagonista;
     private int itemId;
 
-    // Efeito sobre atributo do protagonista
+    /**
+     * Cria um efeito relacionado a um atributo do protagonista.
+     *
+     * @param tipo tipo do efeito
+     * @param alvo atributo que será afetado
+     * @param valor valor da alteração
+     */
+
     public Efeito(String tipo, String alvo, int valor) {
         this.tipo = tipo;
         this.alvo = alvo;
@@ -16,7 +29,12 @@ public class Efeito {
         this.itemId = -1;
     }
 
-    // Efeito sobre confiança de um NPC
+    /**
+     * Cria um efeito que altera a confiança de um NPC.
+     *
+     * @param npc NPC cuja confiança será alterada
+     * @param valor valor da alteração na confiança
+     */
     public Efeito(NPC npc, int valor) {
         this.tipo = "CONFIANCA";
         this.alvo = null;
@@ -24,6 +42,12 @@ public class Efeito {
         this.npc = npc;
         this.itemId = -1;
     }
+
+    /**
+     * Cria um efeito que adiciona um item ao inventário do protagonista.
+     *
+     * @param itemId identificador (indice) do item que será adicionado ao ivnentario
+     */
     public Efeito(int itemId) {
         this.tipo = "ADICIONAR_ITEM";
         this.alvo = null;
