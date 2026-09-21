82f56e7d5fc7dd3053f3c9e06fc6ee0042e3130a
Levi <levisenandrade@gmail.com>
2026-09-20 23:47:09 -0300

    documentação

--- src/NPC.java
+++ src/NPC.java
@@ -1,3 +1,11 @@
+/**
+ * classe que representa os personagens não jogáveis da narrativa do jogo
+ *
+ * O único atributo do NPC é a confiança em relação ao protagonista,
+ * que é mútua; não existe diferença entre a confiança do protagonista para o NPC X e o inverso.
+ * O atributo pode ser alterado durante a partida e permanece limitado entre 0 e 100.
+ *
+ */
 public class NPC extends Personagem {
     private int confianca;
 
@@ -7,6 +15,14 @@ public class NPC extends Personagem {
         this.confianca = confiancaInicial;
     }
 
+
+    /**
+     * muda o valor da relação de confiança entre o protagonista e o npc
+     *
+     * esse método, através do outro método descrito abaixo "validaConfiança" já se encarrega
+     * de manter o valor variando entre 0 e 100
+     * @param quantidade valor que será acrescentado ou subtraído da confiança
+     */
     public void alterarConfianca(int quantidade){
         this.confianca += quantidade;
         validaConfianca();
@@ -16,6 +32,9 @@ public class NPC extends Personagem {
         return this.confianca;
     }
 
+    /**
+     * método que mantem o nível de confiança dentro dos limites permitidos
+     */
     private void validaConfianca() {
         if (confianca > 100) {
             confianca = 100;
