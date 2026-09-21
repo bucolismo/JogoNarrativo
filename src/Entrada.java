3a6832279914e2ec5c98a4b77e2d7038a321971a
Levi <levisenandrade@gmail.com>
2026-09-20 23:44:25 -0300

    documentacão

--- src/Entrada.java
+++ src/Entrada.java
@@ -1,3 +1,11 @@
+/**
+ * buscando atingir o padrão MVC, esse método é o responsável pela entrada
+ * de dados do usuario
+ *
+ * Utiliza um objeto Scanner para realizar a leitura de valores inteiros
+ * e de strings digitados pelo jogador
+ */
+
 import java.util.Scanner;
 
 public class Entrada {
@@ -7,12 +15,22 @@ public class Entrada {
         scanner = new Scanner(System.in);
     }
 
+    /**
+     * Lê um número inteiro informado pelo usuário e remove a quebra de linha
+     *
+     * @return retorna o número inteiro que foi informado pelo jogador
+     */
     public int lerInteiro() {
         int numero = scanner.nextInt();
         scanner.nextLine(); // remove o Enter do buffer
         return numero;
     }
 
+    /**
+     * Lê uma linha de texto informada pelo usuário.
+     *
+     * @return retonar o texto informado pelo usuário
+     */
     public String lerString(){
         return scanner.nextLine();
     }
