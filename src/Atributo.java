a86a5cb8fcd61d87e407dd071ef5982736b83a05
Levi <levisenandrade@gmail.com>
2026-09-20 23:42:14 -0300

    documentação

--- src/Atributo.java
+++ src/Atributo.java
@@ -1,3 +1,9 @@
+/**
+ * representa os atributo do personagem e seu valor atual
+ *
+ * O valor do atributo varia de 0 ao valor maximo informado na inicialização
+ *
+ */
 public class Atributo {
     private String nome;
     private int valor;
@@ -22,12 +28,24 @@ public class Atributo {
         return this.valor;
     }
 
+    /**
+     * aumenta o valor do atributo pela quantidade informada
+     * se quantidade for menor ou igual a zero não alteraa o valor do atributo
+     *
+     * @param quantidade valor que será acrescentado ao atributo
+     */
     public void aumentaValor(int quantidade) {
         if (quantidade > 0) {
             this.valor += quantidade;
         }
     }
 
+    /**
+     * reduz o valor do atributo pela quantidade informada
+     * no entanto, o valor do atributo não chega a ficar abaixo de zero
+     *
+     * @param quantidade valor que será subtraído do atributo
+     */
     public void reduzValor(int quantidade) {
         if (quantidade > 0) {
             this.valor -= quantidade;
