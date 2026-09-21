9c329a412fe5648602e3afa6a66449716d8ba015
Levi <levisenandrade@gmail.com>
2026-09-20 23:48:27 -0300

    documentação
    e correção no atributo "Violência" que estava com mau funcionamento devido a erro de digitação

--- src/Protagonista.java
+++ src/Protagonista.java
@@ -1,3 +1,10 @@
+/**
+ * é a classe que representa o personagem controlado pelo jogador
+ *
+ * O protagonista possui atributos que representam seu estado durante a
+ * partida (violência/razão/paranoia) e um inventário que armazena os itens disponíveis
+ */
+
 public class Protagonista extends Personagem {
     private Atributo[] atributos;
     private Inventario inventario;
@@ -9,11 +16,21 @@ public class Protagonista extends Personagem {
 
         atributos[0] = new Atributo("Razão", 50, 100);
         atributos[1] = new Atributo("Paranoia", 50, 100);
-        atributos[2] = new Atributo("Violencia", 50, 100);
+        atributos[2] = new Atributo("Violência", 50, 100);
 
         inventario = new Inventario();
     }
 
+    /**
+     * esse método altera o valor de um atributo do protagonista.
+     *
+     * esse método funciona baseado no @param quantidade que é passado,
+     * se a quantidade for positiva adiciona, se a quantidade for negativa
+     * o valor do atributo é reduzido.
+     *
+     * @param nomeAtributo nome do atributo que será alterado
+     * @param quantidade valor que será acrescentado ou subtraído do atributo
+     */
     public void alteraAtributo(String nomeAtributo, int quantidade) {
         for (int i = 0; i < atributos.length; i++) {
             if (atributos[i].getNome().equalsIgnoreCase(nomeAtributo)) {
@@ -37,66 +54,33 @@ public class Protagonista extends Personagem {
         return 0;
     }
 
+    /**
+     * cadastro todos os itens que vão ser utilizados pela história no inventário do protagonista
+     * todos itens são cadastrados porque eles por default são false, ou seja, o jogador precisa
+     * pegar o item em algum momento na história para que ele seja habilitado e adicionados ao invenptario
+     */
     public void cadastrarItens(){
-        Item pistaDeJonas    = new Item("Pista de Jonas", "Pista", "Informação repassada pelo Velho Jonas sobre a fábrica desativada", 6);
-        Item pistaManequim   = new Item("Pista do Manequim", "Pista", "Uma pista plantada por alguém em um manequim no beco", 7);
-        Item laudoAdulterado = new Item("Laudo Adulterado", "Evidência", "Laudo pericial do caso de Gabriel com rasuras propositais", 8);
-        Item diarioDoIrmao   = new Item("Diário de Gabriel", "Prova", "Diário de Gabriel encontrado no esconderijo da fábrica", 10);
-        Item gravacao        = new Item("Gravação de Gabriel", "Prova", "Gravação de voz deixada por Gabriel alertando sobre Daniel", 11);
-        Item radioPolicial   = new Item("Rádio de Frequência Restrita", "Acesso", "Rádio da polícia entregue por Daniel após ganhar sua confiança total", 12);
-        Item pastaOriginal   = new Item("Pasta Original do Laudo", "Evidência", "Laudo original resgatado do Arquivo Morto da delegacia, sem as rasuras oficiais", 13);
-        Item cartaoPrefeitura   = new Item("Cartão de Acesso da Prefeitura", "Acesso", "Cartão magnético timbrado da Prefeitura, encontrado no bolso do incendiário", 15);
-        Item canalGabinete      = new Item("Pista: Canal do Gabinete", "Pista", "Código interno do gabinete municipal usado para dar ordens ao suspeito", 16);
-        Item vinculoVicePrefeito= new Item("Pista: Vínculo com Vice-Prefeito", "Pista", "Confissão de que o pagamento veio do gabinete do vice-prefeito", 17);
-        Item testemunhoVerbal   = new Item("Testemunho Verbal Não-Assinado", "Pista", "Confirmação verbal do envolvimento da administração, sem assinatura", 18);
-        Item nomeAssessor       = new Item("Nome do Assessor", "Pista", "Nome do assessor direto do vice-prefeito, obtido sob pressão", 19);
-        Item pontoEncontro      = new Item("Pista: Ponto de Encontro do Transfuga", "Pista", "Local e horário da entrega do restante do material queimado", 20);
-        Item procuradoAgressao = new Item("Procurado por Agressão", "Status", "Alerta ativo contra você por violência cometida durante a investigação", 21);
-        Item fitaAudio       = new Item("Fita de Áudio Confidencial do Irmão", "Prova", "Fita microcassete recuperada da Central de Custódia", 22);
-        Item diariosSobrado  = new Item("Diários do Sobrado de Gabriel", "Prova", "Diários ocultos sob o assoalho da casa de Gabriel", 23);
-        Item ordemBusca      = new Item("Ordem de Busca e Apreensão Paralela", "Pista", "Pasta de ordens interceptada do carro descaracterizado", 24);
-        Item contaFantasma   = new Item("Mapeamento da Conta Fantasma", "Pista", "Mapa das contas bancárias ligadas à empresa fantasma", 25);
-        Item dossieCadaveres = new Item("Dossiê de Ocultação de Cadáveres", "Prova", "Organograma que liga Daniel a quatro homicídios anteriores", 26);
-        Item panfletos       = new Item("Panfletos de Denúncia Operacional", "Item", "Cópias impressas prontas para distribuição", 27);
-        Item rotaGalerias    = new Item("Rota das Galerias do Centro", "Acesso", "Caminho pelo canal subterrâneo de escoamento", 28);
-        Item pistolaTatica   = new Item("Pistola Tática com Silenciador", "Item", "Arma tomada do agente na penumbra do beco", 29);
-        Item alertaGeral      = new Item("Alerta Geral na Cidade", "Status", "Panfletos com o depoimento e as fotos já circulam publicamente pela cidade", 32);
-        Item ferimentosLeves  = new Item("Ferimentos Leves", "Status", "Dano físico leve sofrido em confronto corpo a corpo", 33);
-        Item armaBranca       = new Item("Arma Branca Adquirida", "Item", "Arma branca tomada de um policial durante confronto", 34);
-        Item gameOver = new Item(("GameOver"),"GameOver","GameOver",-1);
-        Item chaveArquivoMorto = new Item("Chave do Arquivo Morto", "Acesso", "Chave entregue por Daniel ao atingir parceria total com ele", 14);
+        Item cartaoIml = new Item("Cartão do IML", "Acesso", "Um cartão de acesso que permite acessar o IML da cidade", 1);
+        Item chaveMestra = new Item("Chave Mestra do Cartório", "Acesso", "Uma chave estranha com o símbolo do cartório central que permite acessar o prédio de registros", 2);
+        Item cartaoPrefeitura = new Item("Cartão de Acesso da Prefeitura", "Acesso", "Um cartão magnético que permite acessar o prédio da prefeitura municipal", 3);
+        Item chaveCofrePrefeitura = new Item("Chave do Cofre da Prefeitura", "Acesso", "Uma chave de segurança que permite acessar o cofre e o arquivo restrito", 4);
+        Item peDeCabra = new Item("Pé de Cabra", "Acesso", "Uma ferramenta pesada que permite forçar e abrir portas e acessos bloqueados", 5);
+        Item pistaDeJonas = new Item("Pista de Jonas","Acesso","Uma pista que permite seguir outro caminho",6);
+        Item pistaManequim = new Item("Pista do Manequim","Acesso","Uma pista plantada por alguém em um manequim",7);
+        Item laudoAdulterado = new Item("Laudo Adulterado","Evidência","Um laudo adulterado do caso de Gabriel",8);
 
+        inventario.cadastrarItem(cartaoIml);
+        inventario.cadastrarItem(chaveMestra);
+        inventario.cadastrarItem(cartaoPrefeitura);
+        inventario.cadastrarItem(chaveCofrePrefeitura);
+        inventario.cadastrarItem(peDeCabra);
         inventario.cadastrarItem(pistaDeJonas);
         inventario.cadastrarItem(pistaManequim);
         inventario.cadastrarItem(laudoAdulterado);
-        inventario.cadastrarItem(diarioDoIrmao);
-        inventario.cadastrarItem(gravacao);
-        inventario.cadastrarItem(radioPolicial);
-        inventario.cadastrarItem(pastaOriginal);
-        inventario.cadastrarItem(chaveArquivoMorto);
-        inventario.cadastrarItem(cartaoPrefeitura);
-        inventario.cadastrarItem(canalGabinete);
-        inventario.cadastrarItem(vinculoVicePrefeito);
-        inventario.cadastrarItem(testemunhoVerbal);
-        inventario.cadastrarItem(nomeAssessor);
-        inventario.cadastrarItem(pontoEncontro);
-        inventario.cadastrarItem(procuradoAgressao);
-        inventario.cadastrarItem(fitaAudio);
-        inventario.cadastrarItem(diariosSobrado);
-        inventario.cadastrarItem(ordemBusca);
-        inventario.cadastrarItem(contaFantasma);
-        inventario.cadastrarItem(dossieCadaveres);
-        inventario.cadastrarItem(panfletos);
-        inventario.cadastrarItem(rotaGalerias);
-        inventario.cadastrarItem(pistolaTatica);
-        inventario.cadastrarItem(alertaGeral);
-        inventario.cadastrarItem(ferimentosLeves);
-        inventario.cadastrarItem(armaBranca);
-        inventario.cadastrarItem(gameOver);
+
     }
 
     public Inventario getInventario() {
-        return this.inventario;
+        return inventario;
     }
-
 }
